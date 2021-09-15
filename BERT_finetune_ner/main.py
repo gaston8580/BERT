3563405ae42920ef56208b350ebdd4d23435608a
chenxin <chenxin@actionsky.com>
2021-09-15 17:34:17 +0800

    bert code

--- BERT_finetune_ner/main.py
+++ BERT_finetune_ner/main.py
@@ -28,7 +28,7 @@ def main(args):
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser()  #
 
     parser.add_argument("--task", default=None, required=True, type=str, help="The name of the task to train")
     parser.add_argument("--model_dir", default=None, required=True, type=str, help="Path to save, load model")
