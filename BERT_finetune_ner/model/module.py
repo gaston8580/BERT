59224ff621dd7333092a1e46a514c52f8ff0f5dc
Gaston <89963543+Gaston-nlp@users.noreply.github.com>
2021-09-17 21:04:42 +0800

    增加了注释

--- BERT_finetune_ner/model/module.py
+++ BERT_finetune_ner/model/module.py
@@ -1,11 +1,11 @@
 import torch.nn as nn
 
-
+# ner识别的MLP全连接层
 class SlotClassifier(nn.Module):
     def __init__(self, input_dim, num_slot_labels, dropout_rate=0.):
         super(SlotClassifier, self).__init__()
         self.dropout = nn.Dropout(dropout_rate)
-        self.linear = nn.Linear(input_dim, num_slot_labels)
+        self.linear = nn.Linear(input_dim, num_slot_labels)  # nn.Linear(input神经元数量，output神经元数量)
 
     def forward(self, x):
         x = self.dropout(x)
